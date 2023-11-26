b379aaff092ae6cc39b871a414633ffa806118d9
mafajardo23 <mafajardo@davidson.edu>
2023-11-26 12:25:03 -0500

    favicon and pop up message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    openPopup();
+    });
 
+    function openPopup() {
+    document.getElementById('popup').style.display = 'block';
+    }
+
+    function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+    }
+
+        // const context = new (window.AudioContext || window.webkitAudioContext)();
+
+        // const audioElement = new Audio('christmas.mp3');
+        // audioElement.crossOrigin = "anonymous"; // Enable CORS for the audio file
+
+        // const source = context.createMediaElementSource(audioElement);
+        // source.connect(context.destination);
+
+        // audioElement.play().catch(error => {
+        //     console.error("Autoplay was prevented. Please allow autoplay in your browser settings.");
+        //  });
 /* javascript */
